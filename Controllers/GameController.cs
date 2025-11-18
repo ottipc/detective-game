547f229e45edb40ca6ec6a3a0d127dc643c6e96f
Otti B <ottavio.braun@googlemail.com>
2025-11-18 09:08:00 +0100

    ADD Datagrid View

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -13,9 +13,90 @@ namespace Detektivspiel.Controllers
         {
             Suspects = new List<Suspect>
             {
-                new() { Name = "Lady Grey",  Alibi = "Beim Dinner", Motiv = "Eifersucht", IsGuilty = false },
-                new() { Name = "Mr. White",  Alibi = "Im Garten",  Motiv = "Geldnot",    IsGuilty = true  },
-                new() { Name = "Dr. Violet", Alibi = "Im Labor",   Motiv = "Rache",      IsGuilty = false }
+                new() { 
+                    Id = 1,
+                    Name = "Lady Scarlett Grey", 
+                    Alibi = "Beim Dinner mit Gästen", 
+                    Motiv = "Eifersucht auf Vermögen", 
+                    IsGuilty = false,
+                    Age = 42,
+                    Occupation = "Adelige",
+                    Location = "Ballsaal",
+                    Relationship = "Ex-Geliebte",
+                    SuspicionLevel = 7
+                },
+                new() { 
+                    Id = 2,
+                    Name = "Mr. Victor White", 
+                    Alibi = "Im Garten spazieren", 
+                    Motiv = "Schwere Geldnot", 
+                    IsGuilty = true,
+                    Age = 38,
+                    Occupation = "Geschäftsmann",
+                    Location = "Garten",
+                    Relationship = "Geschäftspartner",
+                    SuspicionLevel = 9
+                },
+                new() { 
+                    Id = 3,
+                    Name = "Dr. Elena Violet", 
+                    Alibi = "Im Labor gearbeitet", 
+                    Motiv = "Rache für Verrat", 
+                    IsGuilty = false,
+                    Age = 35,
+                    Occupation = "Wissenschaftlerin",
+                    Location = "Labor",
+                    Relationship = "Ehem. Kollegin",
+                    SuspicionLevel = 6
+                },
+                new() { 
+                    Id = 4,
+                    Name = "Colonel James Mustard", 
+                    Alibi = "In der Bibliothek gelesen", 
+                    Motiv = "Alte Kriegsschuld", 
+                    IsGuilty = false,
+                    Age = 55,
+                    Occupation = "Pensionierter Offizier",
+                    Location = "Bibliothek",
+                    Relationship = "Alter Feind",
+                    SuspicionLevel = 8
+                },
+                new() { 
+                    Id = 5,
+                    Name = "Miss Olivia Peacock", 
+                    Alibi = "Im Salon Tee getrunken", 
+                    Motiv = "Erbschaftsstreit", 
+                    IsGuilty = false,
+                    Age = 28,
+                    Occupation = "Erbin",
+                    Location = "Salon",
+                    Relationship = "Nichte",
+                    SuspicionLevel = 5
+                },
+                new() { 
+                    Id = 6,
+                    Name = "Professor Marcus Plum", 
+                    Alibi = "Im Arbeitszimmer", 
+                    Motiv = "Gestohlene Forschung", 
+                    IsGuilty = false,
+                    Age = 47,
+                    Occupation = "Professor",
+                    Location = "Arbeitszimmer",
+                    Relationship = "Konkurrent",
+                    SuspicionLevel = 7
+                },
+                new() { 
+                    Id = 7,
+                    Name = "Mrs. Isabella Green", 
+                    Alibi = "Im Wintergarten", 
+                    Motiv = "Liebesaffäre", 
+                    IsGuilty = false,
+                    Age = 32,
+                    Occupation = "Hausherrin",
+                    Location = "Wintergarten",
+                    Relationship = "Ehefrau",
+                    SuspicionLevel = 4
+                }
             };
             guilty = Suspects.First(s => s.IsGuilty);
         }
--- Models/Suspect.cs
+++ Models/Suspect.cs
@@ -2,9 +2,15 @@ namespace Detektivspiel.Models
 {
     public class Suspect
     {
+        public int Id { get; set; }
         public string Name { get; set; } = "";
         public string Alibi { get; set; } = "";
         public string Motiv { get; set; } = "";
         public bool IsGuilty { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Relationship { get; set; } = "";
+        public int SuspicionLevel { get; set; } // 1-10
     }
 }
